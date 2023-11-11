2de18a37487fe0d7e7457ca14de8670b1ae2b9dd
rakibaakter <rakibaakter635@gmail.com>
2023-11-11 17:36:52 +0600

    added slider from react-responsive-carousel

--- bistro-boss-client/src/Routes/Router.jsx
+++ bistro-boss-client/src/Routes/Router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
