607858607675883df8ba5e0a8b96be42b0a67599
rakibaakter <rakibaakter635@gmail.com>
2023-11-11 19:24:01 +0600

    added autoplay to banner

--- bistro-boss-client/src/Pages/Home/HomeComponents/Banner/Banner.jsx
+++ bistro-boss-client/src/Pages/Home/HomeComponents/Banner/Banner.jsx
@@ -11,7 +11,7 @@ import banner6 from "../../../../assets/home/06.png";
 const Banner = () => {
   return (
     <div>
-      <Carousel className="text-center">
+      <Carousel autoPlay="true" className="text-center">
         <div>
           <img src={banner1} />
         </div>
