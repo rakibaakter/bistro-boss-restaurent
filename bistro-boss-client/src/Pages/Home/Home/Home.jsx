c9951921c130c73afb1f7dfedd6bfa6b927069a6
rakibaakter <rakibaakter635@gmail.com>
2023-11-11 21:06:01 +0600

    added category slider

--- bistro-boss-client/src/Pages/Home/Home/Home.jsx
+++ bistro-boss-client/src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../HomeComponents/Banner/Banner";
+import CategorySlider from "../HomeComponents/Categories/CategorySlider";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <CategorySlider />
     </div>
   );
 };
